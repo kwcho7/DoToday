3b550177dc4d624505e6c8fec43b751b8ae04064
chokyuwon <kwcho@mycreditchain.io>
2019-07-03 15:49:46 +0900

    added. launcherViewModel testcase

---
+++ app/src/androidTest/java/kr/co/cools/today/ui/launcher/LauncherViewModelTest.kt
@@ -0,0 +1,82 @@
+package kr.co.cools.today.ui.launcher
+
+import android.content.Context
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import io.reactivex.Single
+import kotlinx.coroutines.*
+import kr.co.cools.today.repo.dao.TodoDao
+import kr.co.cools.today.repo.entities.TodoEntity
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Mockito.*
+import org.mockito.MockitoAnnotations
+
+@RunWith(AndroidJUnit4::class)
+class LauncherViewModelTest {
+
+    private val context: Context = InstrumentationRegistry.getInstrumentation().context
+
+    @get: Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Mock
+    lateinit var observer: Observer<LauncherViewModel.LauncherState>
+
+    @Mock
+    lateinit var todoDao: TodoDao
+
+    private val launcherInteractor: LauncherInteractor by lazy {
+        spy(LauncherInteractor(context = context, todoDao = todoDao))
+    }
+
+    @Before
+    fun setup() {
+        MockitoAnnotations.initMocks(this)
+    }
+
+    @After
+    fun tearDown() {
+        Mockito.framework().clearInlineMocks()
+    }
+
+    // 1. 할일이 정의되지 않았으면 목록 생성으로 이동한다.
+    @Test
+    fun forwardTodoListIfEmptyTodo() = runBlocking<Unit> {
+        Mockito.`when`(todoDao.getAll()).thenReturn(Single.just(emptyList()))
+        val launcherViewModel = LauncherViewModel(launcherInteractor)
+        CoroutineScope(Dispatchers.Main).launch {
+            launcherViewModel.launcherState.observeForever(observer)
+            launcherViewModel.updateLauncherState()
+        }.join()
+
+        delay(100)
+        verify(observer, times(1)).onChanged(LauncherViewModel.LauncherState.ShowProgress)
+        verify(observer, times(1)).onChanged(LauncherViewModel.LauncherState.HideProgress)
+        verify(observer, times(1)).onChanged(LauncherViewModel.LauncherState.StartTodoListActivity)
+    }
+
+    // 2. 할일이 정의되어 있으면 할일 목록으로 이동한다.
+    @Test
+    fun forwardJobListIfHasTodo() = runBlocking<Unit> {
+        Mockito.`when`(todoDao.getAll()).thenReturn(Single.just(listOf(TodoEntity())))
+        val launcherViewModel = LauncherViewModel(launcherInteractor)
+        CoroutineScope(Dispatchers.Main).launch {
+            launcherViewModel.launcherState.observeForever(observer)
+            launcherViewModel.updateLauncherState()
+        }.join()
+
+        delay(100)
+        verify(observer, times(1)).onChanged(LauncherViewModel.LauncherState.ShowProgress)
+        verify(observer, times(1)).onChanged(LauncherViewModel.LauncherState.HideProgress)
+        verify(observer, times(1)).onChanged(LauncherViewModel.LauncherState.StartJobListActivity)
+    }
+}
+
--- app/src/main/java/kr/co/cools/today/ui/launcher/LauncherInteractor.kt
+++ app/src/main/java/kr/co/cools/today/ui/launcher/LauncherInteractor.kt
@@ -5,14 +5,11 @@ import io.reactivex.Single
 import kr.co.cools.today.repo.dao.TodoDao
 import javax.inject.Inject
 
-class LauncherInteractor @Inject constructor(val context: Context, var todoDao: TodoDao) {
+open class LauncherInteractor @Inject constructor(val context: Context, var todoDao: TodoDao) {
 
-    fun hasTodoEntity(): Single<Boolean> {
+    open fun hasTodoEntity(): Single<Boolean> {
         return todoDao.getAll()
-            .flatMap {
-                Single.just(it.isNotEmpty())
-            }
-
+            .map { it.isNotEmpty() }
     }
 }
 
